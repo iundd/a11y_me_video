c72755053912928884cfb1a072c7b9664c60fb7e
Robert Pudenz <opensource@i-d.de>
2022-05-15 16:27:22 +0200

    [DOC] change Settings.cfg, project title

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,14 +1,14 @@
 <?php
 
 $EM_CONF[$_EXTKEY] = array (
-  'title' => 'a11y_me_video',
+  'title' => 'Accessible video player based on MediaElement.js',
   'description' => 'Accessible video player based on MediaElement.js and A11y Plugin and Fluid. The new content elements support mp4, webm, subtitles, sign language video, audio description, poster image, text alternative, etc.',
   'category' => 'fe',
   'author' => 'Robert Pudenz',
   'author_email' => 'opensource@i-d.de',
   'author_company' => '.i-D. internet + Design GmbH & Co. KG',
   'state' => 'beta',
-  'version' => '1.0.0',
+  'version' => '1.0.1',
   'constraints' =>
   array (
     'depends' =>
