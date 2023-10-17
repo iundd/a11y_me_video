479a007bc204e886111bcd41918e22f5296c6075
iD Seyfferth <c.seyfferth@i-d.de>
2023-10-17 16:07:24 +0200

    [TASK] switch namespace for icons

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,22 +3,22 @@ defined('TYPO3') || die();
 
 call_user_func(function () {
 
-// Register content element icons
-$iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
-$iconRegistry->registerIcon(
-    'tx_a11ymevideo_videoplayer',
-    \TYPO3\CMS\Core\Imaging\IconProvider\FontawesomeIconProvider::class,
-    [
-        'name' => 'film',
-    ]
-);
-$iconRegistry->registerIcon(
-    'tx_a11ymevideo_videoplayer2',
-    \TYPO3\CMS\Core\Imaging\IconProvider\FontawesomeIconProvider::class,
-    [
-        'name' => 'film',
-    ]
-);
+    // Register content element icons
+    $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
+    $iconRegistry->registerIcon(
+        'tx_a11ymevideo_videoplayer',
+        \FriendsOfTYPO3\FontawesomeProvider\Imaging\IconProvider\FontawesomeIconProvider::class,
+        [
+            'name' => 'film',
+        ]
+    );
+    $iconRegistry->registerIcon(
+        'tx_a11ymevideo_videoplayer2',
+        \FriendsOfTYPO3\FontawesomeProvider\Imaging\IconProvider\FontawesomeIconProvider::class,
+        [
+            'name' => 'film',
+        ]
+    );
 
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('
         @import "EXT:a11y_me_video/Configuration/TsConfig/Page/NewContentElementWizard.tsconfig"
