265a95b8305b6e4e95e63cc7865bfa8f042caa47
Robert Pudenz <r.pudenz@dreistrom.land>
2024-07-10 10:34:39 +0200

    [TASK] cleanup

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,7 +2,7 @@
 
 $EM_CONF[$_EXTKEY] =  [
     'title' => 'Accessible video player based on MediaElement.js',
-    'description' => 'Accessible video player based on MediaElement.js and A11y Plugin and Fluid. The new content elements support mp4, webm, subtitles, sign language video, audio description, poster image, text alternative, etc.',
+    'description' => 'Accessible video player based on MediaElement.js, A11y Plugin and Fluid. The new content elements support mp4, webm, subtitles, sign language video, audio description, poster image, text alternative, etc.',
     'category' => 'fe',
     'author' => 'Robert Pudenz',
     'author_email' => 'r.pudenz@dreistrom.land',
